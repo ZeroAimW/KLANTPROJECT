2ffdd7ff8dac817feb4250f81b89fc40c850ab42
ZeroAimW <96825068+ZeroAimW@users.noreply.github.com>
2026-05-12 13:38:37 +0200

    Delete Almere50Jaar-main directory

--- Almere50Jaar-main/Almere50Jaar-main/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './styles/theme.css'
-import './styles/fonts.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- Almere50Jaar-main/Almere50Jaar-main/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface TimelineEvent {
-  id: string;
-  date: string;
-  title: string;
-  description: string;
-  color: string;
-  imageUrl?: string;
-}
